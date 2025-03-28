8272e0f8214c1cc8034b64e81333ac0985f3c589
noahpachec <noahpachec@gmail.com>
2025-03-28 20:26:42 +1000

    Saving projects

--- scripts.js
+++ scripts.js
@@ -5,41 +5,55 @@ let value = "";
 
 document.getElementById("b1").addEventListener("click", function() {
     expression += "1";
+    document.getElementById("screen-text").innerText = expression;
     console.log(expression);
 })
 
 document.getElementById("b2").addEventListener("click", function() {
     expression += "2";
+    document.getElementById("screen-text").innerText = expression;
     console.log(expression)
 })
 
 document.getElementById("b3").addEventListener("click", function() {
     expression += "3";
+    document.getElementById("screen-text").innerText = expression;
     console.log(expression)
 })
 
 document.getElementById("b4").addEventListener("click", function() {
     expression += "4";
+    document.getElementById("screen-text").innerText = expression;
     console.log(expression)
 })
 
 document.getElementById("b5").addEventListener("click", function() {
     expression += "5";
+    document.getElementById("screen-text").innerText = expression;
     console.log(expression)
 })
 
 document.getElementById("b6").addEventListener("click", function() {
     expression += "6";
+    document.getElementById("screen-text").innerText = expression;
     console.log(expression)
 })
 
 document.getElementById("b7").addEventListener("click", function() {
+    expression += "7";
+    document.getElementById("screen-text").innerText = expression;
+    console.log(expression)
+})
+
+document.getElementById("b8").addEventListener("click", function() {
     expression += "8";
+    document.getElementById("screen-text").innerText = expression;
     console.log(expression)
 })
 
 document.getElementById("b9").addEventListener("click", function() {
     expression += "9";
+    document.getElementById("screen-text").innerText = expression;
     console.log(expression)
 })
 
@@ -47,6 +61,7 @@ document.getElementById("b9").addEventListener("click", function() {
 
 document.getElementById("bac").addEventListener("click", function() {
     expression = "";
+    document.getElementById("screen-text").innerText = expression;
     console.log(expression)
 })
 
@@ -54,28 +69,41 @@ document.getElementById("bac").addEventListener("click", function() {
 
 document.getElementById("b+").addEventListener("click", function() {
     expression += "+";
+    document.getElementById("screen-text").innerText = expression;
     console.log(expression)
 })
 
 document.getElementById("b-").addEventListener("click", function() {
     expression += "-";
+    document.getElementById("screen-text").innerText = expression;
     console.log(expression)
 })
 
 document.getElementById("b÷").addEventListener("click", function() {
     expression += "/";
+    document.getElementById("screen-text").innerText = expression;
     console.log(expression)
 })
 
 document.getElementById("bx").addEventListener("click", function() {
     expression += "*";
+    document.getElementById("screen-text").innerText = expression;
     console.log(expression)
 })
 
 document.getElementById("b^2").addEventListener("click", function() {
-    expression += "^2";
+    expression += "**2";
+    document.getElementById("screen-text").innerText = expression;
     console.log(expression)
 })
 
 
 // = //
+
+document.getElementById("b=").addEventListener("click", function() {
+    expression = eval(expression)
+    document.getElementById("screen-text").innerText = expression;
+    console.log(eval(expression))
+})
+
+// display //
